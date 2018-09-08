39f9406cef1e9c96d4c1e49441556dd3485a40c9
Tyler Molander <tylermolander@gmail.com>
2018-09-08 18:34:15 -0400

    ForLoop

--- Assets/GraphLesson/Graph.cs
+++ Assets/GraphLesson/Graph.cs
@@ -5,13 +5,13 @@ public class Graph : MonoBehaviour
 {
     public Transform pointPrefab;
 
-    void Awake ()
+    void Awake()
     {
-        Transform point = Instantiate(pointPrefab);
-        point.localPosition = Vector3.right;
-
-        point = Instantiate(pointPrefab);           //don't need to declare var point again
-        point.localPosition = Vector3.right * 2f;
+        int i = 0;
+        while (i++ < 10)
+        {
+            Transform point = Instantiate(pointPrefab);
+            point.localPosition = Vector3.right * i;
+        }
     }
-
 }
